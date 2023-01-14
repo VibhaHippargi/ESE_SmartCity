2eac78998cff9cce183a89c5f26fd7701f6aa9c8
SupadmaKadabi <116349331+SupadmaKadabi@users.noreply.github.com>
2023-01-14 17:45:18 +0100

    Code and header files for Accident occurred Scenario.

--- State.cpp
+++ State.cpp
@@ -1,100 +1,212 @@
-#include <pthread.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include <time.h>
-#include <stdio.h>
 #include <time.h>
 #include <dos.h>
 #include <windows.h>
 #include <complex>
 #include <chrono>
-#include <thread>
+#include<string.h>
+#include "State.h"
+
 using namespace std;
 
 
-int main()
 
+Vehicle::Vehicle() {
+
+}
+
+
+
+void Vehicle::update_speed(State current)
 {
-	enum class State
+	if (current == State::decelerating)
 	{
-		driving_normal=1,
-		decelerating,
-		changed_path,
-		changed_lane_direction
-	};
+		cout << "Acknowledgement: Vehicle decelerating\n";
+	}
+}
 
-	
-	int distance_of_ahead_vehicle;
-	
-	char accident_occured,alternate_path_available;
-	State previous_state = State::driving_normal;
-	//State current_state = State::decelerating;
-	State current_state= previous_state;
-	
-	for (int i = 0; i <=2; i++)
+void Vehicle::drive_normal(State current)
+{
+	if (current == State::driving_normal)
 	{
-		//cout << "iteration: "<<i << endl;
+		cout << "Acknowledgement: Vehicle continue same path\n";
+	}
+}
 
-		switch (current_state) {
+void Vehicle::update_direction(State current, char direction_avail)
+{
+	if (direction_avail == 't') {
+		cout << "Acknowledgement: Vehicle Changed Path\n";
+	}
+}
 
-		case State::driving_normal:
-			cout << "please give details" << endl;
-			cout << "distance_ahead in int value, Accident_occured type t or f ,Alternate path available type t or f" << endl;
-			cin >> distance_of_ahead_vehicle;
-			cin >> accident_occured;
-			cin >> alternate_path_available;
-
-			//cout << "current State is driving normal"<<endl;
-			if ((accident_occured == 't') && (distance_of_ahead_vehicle <= 20))
-			{
-				current_state = State::decelerating;
-				cout << "current State is decelerating"<<endl;
-				//cout << State::decelerating << endl;
-			}
-			else
-			{
-				cout << "current State is driving normal" << endl;
-			}
-
-			break;
-
-		case State::decelerating:
-			//cout << "current_state is decelerating" << endl;
-			Sleep(5000);
-			if (alternate_path_available == 't')
-			{
-				current_state = State::changed_path;
-				cout << "current_state is changed path" << endl;
-			}
-			else
-			{
-				current_state = State::changed_lane_direction;
-				cout << "current_state is changed lane direction" << endl;
-			}
-			break;
-
-		case State::changed_path:
-			//cout << "current_state is changed path" << endl;
-			Sleep(5000);
-			current_state = State::driving_normal;
-			cout << "current State is driving normal" << endl;
-			cout << "ONE COMPLETE FLOW DONE" << endl;
-			break;
-
-		case State::changed_lane_direction:
-			//cout << "current_state is changed lane direction" << endl;
-			Sleep(5000);
-			current_state = State::driving_normal;
-			cout << "current State is driving normal" << endl;
-			cout << "ONE COMPLETE FLOW DONE" << endl;
-			break;
+void Vehicle::update_lane(State current, char lane_avail)
+{
+	if (lane_avail == 't') {
+		cout << "Acknowledgement: Vehicle Changed Lane Direction\n";
+	}
+}
 
 
-		default:
+	Accident_Scenario:: Accident_Scenario()
+	{
+		this->distance_of_ahead_vehicle = 0;
+		this->alternate_path_available = 'f';
+		this->accident_occured = 'f';
+	}
+
+	Accident_Scenario::Accident_Scenario(int dist, char accident, char path) {
+		this->distance_of_ahead_vehicle = dist;
+		this->accident_occured = accident;
+		this->alternate_path_available = path;
+	}
+	 void Accident_Scenario::setdistance_of_ahead_vehicle(int dist)
+	{
+		this->distance_of_ahead_vehicle = dist;
+	}
+	int Accident_Scenario::getdistance_of_ahead_vehicle()
+	{
+		return(this->distance_of_ahead_vehicle);
+	}
+	void Accident_Scenario::setaccident_occured(char alt)
+	{
+		this->accident_occured = alt;
+	}
+	char Accident_Scenario::getaccident_occured()
+	{
+		return(this->accident_occured);
+	}
+	void Accident_Scenario::setalternate_path_available(char alt)
+	{
+		this->alternate_path_available = alt;
+	}
+	char Accident_Scenario::getalternate_path_available()
+	{
+		return(this->alternate_path_available);
+	}
+	
+ State Accident_Scenario::State_Changes(State current){
+	 Vehicle v1;
+	switch (current) {
+		
+	case State::driving_normal:		
+
+		
+		if ((accident_occured == 't') && (distance_of_ahead_vehicle <= 50))
+		{
+			current = State::decelerating;
+			cout << "Message: Vehicle decelerate" << endl;		
+		}
+		else
+		{
+			cout << "Message: Vehicle drive normal" << endl;
+		}
+		return (current);
+		break;
+
+	case State::decelerating:
+		
+		Sleep(5000);
+		if (alternate_path_available == 't')
+		{
+			current = State::changed_path;
+			cout << "Message: Vehicle change path" << endl;
+		}
+		else
+		{
+			current = State::changed_lane_direction;
+			cout << "Message: Vehicle change lane direction" << endl;
+		}
+		return (current);
+		break;
+
+	case State::changed_path:
+		
+		Sleep(5000);
+		current = State::driving_normal;
+		cout << "Message: Vehicle drive normal" << endl;
+		
+		return (current);
+		break;
+
+	case State::changed_lane_direction:
+		
+		Sleep(5000);
+		current = State::driving_normal;
+		cout << "Message: Vehicle drive normal" << endl;
+		
+		return (current);
+		break;
+
+	default:
+		
+		break;
+	}
+
+	}
+	  
+
+
+
+
+int main()
+{
+	int distance_of_ahead_vehicle = 0;
+	char accident_occur = 'f';
+	char path_available = 'f';
+
+	State current_state = State::driving_normal;
+	Accident_Scenario a1(distance_of_ahead_vehicle,accident_occur,path_available);
+	Vehicle v1;
+	cout << "Press q to Quit, 't' for True and 'f' for False \n";
+	while (true)
+	{		
+		cout << "Enter distance from next vehicle: ";
+		if (!(cin>>distance_of_ahead_vehicle))
+		{
+			cout << "Integer numbers only allowed";
 			break;
 		}
-
-		previous_state = current_state;
-	}
+		cout << "Enter Accident possiblility: ";
+		cin >> accident_occur;
+		if (accident_occur == 'q')
+		{
+			break;
+		}
+		cout << "Enter if path available: ";
+		cin >> path_available;
+		if (path_available == 'q')
+		{
+			break;
+		}
+		a1.setdistance_of_ahead_vehicle(distance_of_ahead_vehicle);
+		a1.setalternate_path_available(path_available);
+		a1.setaccident_occured(accident_occur);
+		
+		State _state = a1.State_Changes(current_state);
+		while (_state != State::driving_normal)
+		{
+			
+			if (_state == State::decelerating)
+			{
+				v1.update_speed(_state);
+			}
+			if (_state == State::changed_path)
+			{
+				v1.update_direction(_state, path_available);
+			}
+			if (_state == State::changed_lane_direction)
+			{
+				v1.update_lane(_state, 't');
+			}
+			_state = a1.State_Changes(_state);
+		}	
+		if (_state == State::driving_normal)
+		{
+			v1.drive_normal(_state);
+		}
+	}	
 }
---
+++ State.h
@@ -0,0 +1,44 @@
+#ifndef State_h
+#define State_h
+
+
+
+	enum class State
+	{
+		driving_normal = 1,
+		decelerating,
+		changed_path,
+		changed_lane_direction
+	};
+	class Vehicle
+	{
+	public:
+		Vehicle();
+		void update_speed(State current);
+		void drive_normal(State current);
+		void update_direction(State current, char direction_avail);
+		void update_lane(State current, char lane_avail);
+	};
+	class Accident_Scenario
+	{
+	public:
+
+		int distance_of_ahead_vehicle;
+		char accident_occured;
+		char alternate_path_available;
+		Accident_Scenario();
+		Accident_Scenario(int distance_of_ahead_vehicle, char accident_occured, char alternate_path_available);
+		void setdistance_of_ahead_vehicle(int dist);
+		int getdistance_of_ahead_vehicle();
+		void setaccident_occured(char alt);
+		char getaccident_occured();
+		void setalternate_path_available(char alt);
+		char getalternate_path_available();
+		State State_Changes(State current);
+	};
+
+	
+	extern Vehicle veh;
+	extern Accident_Scenario acc;
+
+#endif // !State_h
